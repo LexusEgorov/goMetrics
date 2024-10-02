1c6d26da67babe11444388694301f0421433a487
lexusegorov <alexey.egorov.job@gmail.com>
2024-10-02 23:12:39 +0700

    feat: add server to 8080 port

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func testHandle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	response := Response{Message: "Hello, Go!"}
+
+	responseJSON, err := json.Marshal(response)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Write(responseJSON)
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc(`/`, testHandle)
+
+	http.ListenAndServe(":8080", mux)
+}
