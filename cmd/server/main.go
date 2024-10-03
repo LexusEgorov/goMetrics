6ed593e820c562735c5ba5e152ac72a49754cb05
lexusegorov <alexey.egorov.job@gmail.com>
2024-10-03 09:44:42 +0700

    feat: add metrics updating

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,33 +1,122 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 )
 
-type Response struct {
-	Message string `json:"message"`
+type metricName string
+
+type Metric struct {
+	gauge   float64
+	counter int64
 }
 
-func testHandle(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
+func (m *Metric) updateGauge(gaugeValue float64) {
+	m.gauge = gaugeValue
+}
 
-	response := Response{Message: "Hello, Go!"}
+func (m *Metric) updateCounter(counterValue int64) {
+	m.counter += counterValue
+}
 
-	responseJSON, err := json.Marshal(response)
+type MemStorage struct {
+	metrics map[metricName]Metric
+}
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+func (m *MemStorage) addMetric(metricName metricName) Metric {
+	createdMetric := Metric{
+		gauge:   0,
+		counter: 0,
 	}
 
-	w.Write(responseJSON)
+	m.metrics[metricName] = createdMetric
+
+	return createdMetric
+}
+
+func (m MemStorage) getMetric(metricName metricName) *Metric {
+	metrics := m.metrics
+
+	if len(metrics) == 0 {
+		createdMetric := m.addMetric(metricName)
+		return &createdMetric
+	}
+
+	foundMetric, isFound := m.metrics[metricName]
+
+	if !isFound {
+		foundMetric = m.addMetric(metricName)
+	}
+
+	return &foundMetric
+}
+
+func (m *MemStorage) setMetric(mName metricName, metric Metric) {
+	m.metrics[mName] = metric
+}
+
+var storage = MemStorage{
+	metrics: make(map[metricName]Metric),
+}
+
+func updateMetric(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	mName := r.PathValue("metricName")
+	mType := r.PathValue("metricType")
+	mValue := r.PathValue("metricValue")
+
+	fmt.Printf("Name: %s\nType: %s\nValue: %s\n", mName, mType, mValue)
+
+	if mName == "" || mValue == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	currentMetric := storage.getMetric(metricName(mName))
+
+	switch mType {
+	case "gauge":
+		value, err := strconv.ParseFloat(mValue, 64)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		currentMetric.updateGauge(value)
+		storage.setMetric(metricName(mName), *currentMetric)
+
+	case "counter":
+		value, err := strconv.ParseInt(mValue, 0, 64)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		fmt.Println(value)
+		currentMetric.updateCounter(value)
+		storage.setMetric(metricName(mName), *currentMetric)
+
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(storage.metrics)
+	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc(`/`, testHandle)
+	mux.HandleFunc(`/update/{metricType}/{metricName}/{metricValue}`, updateMetric)
 
 	http.ListenAndServe(":8080", mux)
 }
