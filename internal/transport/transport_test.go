13611b9ed1867d86262d44b71cdbb7da196abe84
lexusegorov <alexey.egorov.job@gmail.com>
2024-10-14 02:05:02 +0700

    feat: add tests. WIP.

---
+++ internal/transport/transport_test.go
@@ -0,0 +1,100 @@
+package transport
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestTransportLayer_UpdateMetric(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		tr   TransportLayer
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.tr.UpdateMetric(tt.args.w, tt.args.r)
+		})
+	}
+}
+
+func TestTransportLayer_GetMetric(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		tr   TransportLayer
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.tr.GetMetric(tt.args.w, tt.args.r)
+		})
+	}
+}
+
+func TestTransportLayer_GetMetrics(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		tr   TransportLayer
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.tr.GetMetrics(tt.args.w, tt.args.r)
+		})
+	}
+}
+
+func TestTransportLayer_SendMetric(t *testing.T) {
+	type args struct {
+		metricName  string
+		metricType  string
+		metricValue string
+	}
+	tests := []struct {
+		name string
+		tr   TransportLayer
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.tr.SendMetric(tt.args.metricName, tt.args.metricType, tt.args.metricValue)
+		})
+	}
+}
+
+func TestCreateTransport(t *testing.T) {
+	tests := []struct {
+		name string
+		want Transporter
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CreateTransport(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CreateTransport() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
