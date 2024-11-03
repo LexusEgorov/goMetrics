dd2a24c82ebf6a60521aa19df608317359266f1f
lexusegorov <alexey.egorov.job@gmail.com>
2024-11-03 19:59:51 +0700

    feat: add to request json format for server and agent

--- internal/transport/transport.go
+++ internal/transport/transport.go
@@ -1,6 +1,8 @@
 package transport
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -10,6 +12,13 @@ import (
 
 	"github.com/LexusEgorov/goMetrics/internal/services/storage"
 )
+
+type Metric struct {
+	ID    string   `json:"id"`
+	MType string   `json:"type"`
+	Delta *int64   `json:"delta,omitempty"`
+	Value *float64 `json:"value,omitempty"`
+}
 
 type Storager interface {
 	AddGauge(key storage.MetricName, value storage.Gauge)
@@ -24,34 +33,35 @@ type transportLayer struct {
 }
 
 func (t transportLayer) UpdateMetric(w http.ResponseWriter, r *http.Request) {
-	mName := r.PathValue("metricName")
-	mType := r.PathValue("metricType")
-	mValue := r.PathValue("metricValue")
+	var currentMetric Metric
+	var buf bytes.Buffer
 
-	if mName == "" || mValue == "" {
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if err = json.Unmarshal(buf.Bytes(), &currentMetric); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	mName := currentMetric.ID
+	mType := currentMetric.MType
+	mValue := currentMetric.Value
+	mDelta := currentMetric.Delta
+
+	if mName == "" || (mValue == nil && mDelta == nil) {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
 	switch mType {
 	case "gauge":
-		value, err := strconv.ParseFloat(mValue, 64)
-
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		t.storage.AddGauge(storage.MetricName(mName), storage.Gauge(value))
+		t.storage.AddGauge(storage.MetricName(mName), storage.Gauge(*mValue))
 	case "counter":
-		value, err := strconv.ParseInt(mValue, 0, 64)
-
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		t.storage.AddCounter(storage.MetricName(mName), storage.Counter(value))
+		t.storage.AddCounter(storage.MetricName(mName), storage.Counter(*mDelta))
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -135,12 +145,50 @@ func (t transportLayer) SendMetric(host, metricName, metricType, metricValue str
 
 	client := resty.New()
 
-	_, err := client.R().
-		SetHeader("Content-Type", "text/plain").
+	currentMetric := Metric{
+		ID:    metricName,
+		MType: metricType,
+	}
+
+	if metricType == "gauge" {
+		gaugeValue, err := strconv.ParseFloat(metricValue, 64)
+
+		if err != nil {
+			fmt.Printf("ERROR: %s\n", err)
+			return
+		}
+
+		currentMetric.Value = &gaugeValue
+	} else if metricType == "counter" {
+		counterValue, err := strconv.Atoi(metricValue)
+
+		if err != nil {
+			fmt.Printf("ERROR: %s\n", err)
+			return
+		}
+
+		deltaValue := int64(counterValue)
+		currentMetric.Delta = &deltaValue
+	} else {
+		fmt.Printf("ERROR: unknown metricType\n")
+		return
+	}
+
+	body, err := json.Marshal(currentMetric)
+
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		return
+	}
+
+	_, err = client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(body).
 		Post(url)
 
 	if err != nil {
-		fmt.Printf("ERROR: %s", err)
+		fmt.Printf("ERROR: %s\n", err)
+		return
 	}
 }
 
