1d9f7fa627a439d456e1f8a8d6e154654c32d070
lexusegorov <alexey.egorov.job@gmail.com>
2025-02-24 20:58:01 +0700

    I __ a word

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/LexusEgorov/goMetrics/internal/config"
 	"github.com/LexusEgorov/goMetrics/internal/runners"
 )
+
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+func defaultIfEmpty(value string) string {
+	if value == "" {
+		return "N/A"
+	}
+	return value
+}
+
+func init() {
+	fmt.Printf("Build version: %s\n", defaultIfEmpty(buildVersion))
+	fmt.Printf("Build date: %s\n", defaultIfEmpty(buildDate))
+	fmt.Printf("Build commit: %s\n", defaultIfEmpty(buildCommit))
+}
 
 func main() {
 	server := runners.NewServer()
